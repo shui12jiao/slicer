f088f3dbf4e36331a2d018b502f417f4bec61ea9
sming <mengsiming77@gmail.com>
2025-04-01 17:40:31 +0800

    fix env, getSupportedKpis通过测试

---
+++ monitor/request_translator_test.go
@@ -0,0 +1,23 @@
+package monitor
+
+import (
+	"slicer/util"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const NODE_IP = "172.18.0.3"
+
+var monitor = NewMonitor(util.Config{
+	MonarchThanosURI:            "http://" + NODE_IP + ":31004",
+	MonarchRequestTranslatorURI: "http://" + NODE_IP + ":30700",
+	MonitorTimeout:              255,
+})
+
+func TestGetSupportedKpis(t *testing.T) {
+	supportedKpis, err := monitor.GetSupportedKpis()
+	require.NoError(t, err)
+	require.NotEmpty(t, supportedKpis)
+	require.Equal(t, supportedKpis[0].KpiName, "slice_throughput")
+}
